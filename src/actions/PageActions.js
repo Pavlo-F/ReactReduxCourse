08f9b85d67f59238fb8a6d6871c1fb30b6611ced
p.filatov@i-sys.ru <p.filatov@i-sys.ru>
2018-10-23 13:53:23 +0400

    [+] Middleware

---
+++ src/actions/PageActions.js
@@ -0,0 +1,8 @@
+﻿export const SET_YEAR = 'SET_YEAR'
+
+export function setYear(year) {
+    return {
+        type: SET_YEAR,
+        payload: year,
+    }
+}
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,5 @@
-﻿import { createStore } from 'redux'
-import { rootReducer, initialState } from '../reducers'
+﻿import { createStore, applyMiddleware } from 'redux'
+import { rootReducer } from '../reducers'
+import logger from 'redux-logger'
 
-export const store = createStore(rootReducer, initialState)
+export const store = createStore(rootReducer, applyMiddleware(logger))
